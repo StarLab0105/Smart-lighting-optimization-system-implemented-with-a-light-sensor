33e0405e0269e41d0dba4876c63b519b783e2bcb
gunwoo999 <ndy87020684@gmail.com>
2025-05-31 14:27:10 +0900

    Update lux.py

--- lux.py
+++ lux.py
@@ -20,4 +20,56 @@ for i in lux_data_day:
         lux_data_day[i] *= 0.8
     else:
         set_light = 100
-        lux_data_day[i] = 100
+        lux_data_day[i] *= 1
+
+
+______________________________________________________________________________________________________________________________________________________
+
+import numpy as np
+
+lux_01 = np.random.randint(0, 20)
+lux_02 = np.random.randint(0, 10)
+lux_03 = np.random.randint(0, 15)
+lux_04 = np.random.randint(0, 5)
+lux_05 = np.random.randint(5, 20)
+lux_06 = np.random.randint(20, 100)
+lux_07 = np.random.randint(100, 300)
+lux_08 = np.random.randint(300, 800)
+lux_09 = np.random.randint(800, 1500)
+lux_10 = np.random.randint(1500, 2500)
+lux_11 = np.random.randint(2500, 4000)
+lux_12 = np.random.randint(4000, 6000)
+lux_13 = np.random.randint(3500, 5500)
+lux_14 = np.random.randint(2500, 4500)
+lux_15 = np.random.randint(2000, 3500)
+lux_16 = np.random.randint(1000, 2000)
+lux_17 = np.random.randint(300, 1000)
+lux_18 = np.random.randint(100, 300)
+lux_19 = np.random.randint(0, 10)
+lux_20 = 0
+lux_21 = 0
+lux_22 = 0
+lux_23 = 0
+lux_24 = 0 '
+
+lux_day = [lux_01,lux_02,lux_03,lux_04,lux_05,lux_06,lux_07,lux_08,lux_09,lux_10,lux_11,lux_12,lux_13,lux_14,lux_15,lux_16 ,lux_17,lux_18,lux_19,lux_20,lux_21,lux_22,lux_23,lux_24]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
